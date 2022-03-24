fc3b69d9f84588a2d7023d350832d6973bfe1e5f
Administrator <huoquan00022@163.com>
2022-03-24 14:28:50 +0800

    add  jk

--- netwrok/123.py
+++ netwrok/123.py
@@ -1,4 +1,6 @@
 # -- coding: utf-8 --
+from elasticsearch import Elasticsearch
+from netwrok.Networkconnect import Netsocket
 
 
 es = Elasticsearch([{'host': "172.29.42.98", 'port': 9200}, {'host': "172.29.45.22", 'port': 9200},{'host': "172.29.45.81", 'port': 9201}], timeout=3600)
@@ -10,3 +12,8 @@ class  Essearch:
             "match_all": {}
         },
         "from": "0",
+        "size": "50"
+    }
+        search_result = es.search(index="preprod-2022.03.15", doc_type=None, body=body)
+        search_result = search_result['hits']['total']['value']  # 符合条件数据条目总数
+        return  search_result
