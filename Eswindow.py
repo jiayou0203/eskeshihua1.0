2e0d00dfa0a81f85d80b4c6426f0089e39a51d5e
Administrator <huoquan00022@163.com>
2022-03-25 11:03:41 +0800

    add  index.html

--- Eswindow.py
+++ Eswindow.py
@@ -1,3 +1,4 @@
+# -- coding: utf-8 --
 from flask import Flask
 from flask import render_template
 from  network.Networkconnect import Essocket
--- network/Networkconnect/Essocket.py
+++ network/Networkconnect/Essocket.py
@@ -1,10 +1,14 @@
 # -- coding: utf-8 --
+# -- coding: utf-8 --
+
+
+
 from elasticsearch import Elasticsearch
 import Netsocket
 import requests
 import  ast
 
-es = Elasticsearch([{'host': "172.29.42.98", 'port': 9200}, {'host': "172.29.45.                                                                                                                                                             22", 'port': 9200},{'host': "172.29.45.81", 'port': 9201}], timeout=3600)
+es = Elasticsearch([{'host': "172.29.42.98", 'port': 9200}, {'host': "172.29.45.22", 'port': 9200},{'host': "172.29.45.81", 'port': 9201}], timeout=3600)
 
 class  Essearch:
 
@@ -13,9 +17,16 @@ class  Essearch:
         self.url="http://"+eshost+"/_cat/indices"
         index=requests.get(self.url)
         index.str=index.text
-        index.str=index.str.split()
-        print(type(index.str))
-        return index.str[2]
+        index_zhi=index.str.split()
+       # print(len(index_zhi))
+        index=[]
+        index.append(index_zhi[2])
+        i=2
+        while i<860:
+              index.append(index_zhi[i])
+              i+=10
+              print(i)
+        return index
 
     def Escardno(self):
         body = {
@@ -25,6 +36,6 @@ class  Essearch:
         "from": "0",
         "size": "50"
     }
-        search_result = es.search(index="preprod-2022.03.15", doc_type=None, bod                                                                                                                                                             y=body)
-        search_result = search_result['hits']['total']['value']  # 符合条件数据                                                                                                                                                             条目总数
+        search_result = es.search(index="preprod-2022.03.15", doc_type=None, body=body)
+        search_result = search_result['hits']['total']['value']  # 符合条件数据条目总数
         return  search_result
